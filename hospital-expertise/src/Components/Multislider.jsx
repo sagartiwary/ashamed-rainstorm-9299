673d14cefb04e2c6ef81efa24dc8bed35db62310
prashant_rathour <prathour884@gmail.com>
2023-05-05 14:31:35 +0530

    medicinepage

--- hospital-expertise/src/Components/Multislider.jsx
+++ hospital-expertise/src/Components/Multislider.jsx
@@ -1,6 +1,8 @@
 import { Box, Heading, Image, Text } from "@chakra-ui/react";
 import React, { Component } from "react";
 import Slider from "react-slick";
+import Card, { card } from "./Card";
+import styled from "styled-components";
 
 export default class Multislider extends Component {
   render() {
@@ -82,31 +84,101 @@ export default class Multislider extends Component {
       "https://www.practostatic.com/ecommerce-assets/static/media/home/desktop/cat-9.5029c27e.png",
       "https://www.practostatic.com/ecommerce-assets/static/media/home/desktop/cat-6.aa7d1cba.png",
     ];
+    
+    
     return (
+        <DIV>
       <Box width={"100%"}>
         <Box>
-        <Text textAlign={"left"} ml="5" fontSize={"2xl"} as={"h3"}>CategoriesHealth condition</Text>
-          {" "}
+          <Text textAlign={"left"} ml="5" fontSize={"2xl"} as={"h3"}>
+            CategoriesHealth condition
+          </Text>{" "}
           <Slider {...settings}>
             {images.map((i) => (
               <Box p={3}>
-                <Image src={i} alt="al" />
+                <Image className="img" src={i} alt="al" />
               </Box>
             ))}
           </Slider>
         </Box>
 
         <Box mt={10}>
-        <Text textAlign={"left"} ml="5" fontSize={"2xl"} as={"h3"}>Categories</Text>
+          <Text textAlign={"left"} ml="5" fontSize={"2xl"} as={"h3"}>
+            Categories
+          </Text>
           <Slider {...settings}>
             {images2.map((i) => (
               <Box p={3}>
-                <Image src={i} alt="al" />
+                <Image className="img" src={i} alt="al" />
               </Box>
             ))}
           </Slider>
         </Box>
+       <PauseOnHover/>
+      </Box>
+      </DIV>
+    );
+  }
+}
+
+
+export  class PauseOnHover extends Component {
+  render() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      pauseOnHover: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    };
+
+
+    return (
+      <Box mt={[6,6,6,10]} mb={10}>
+      
+        <Slider {...settings}>
+           { card.map((i)=><Card {...i}/>)}
+        </Slider>
       </Box>
     );
   }
 }
+const DIV=styled.div`
+.img:hover
+{
+    -webkit-transform: scale(1);
+    -moz-transform: scale(1);
+    -o-transform: scale(1);
+    -ms-transform: scale(1);
+    transform: scale(1.1);
+    overflow: hidden;
+}
+`
--- hospital-expertise/src/Pages/Medicines.jsx
+++ hospital-expertise/src/Pages/Medicines.jsx
@@ -1,46 +1,77 @@
-import { Box, Button, Flex, HStack, Heading, Input } from '@chakra-ui/react'
-import React from 'react'
-import styled from 'styled-components'
-import Navbar from '../Components/Navbar'
-import { AiOutlineShoppingCart } from 'react-icons/ai'
+import { Box, Button, Card, Flex, HStack, Heading, Input } from "@chakra-ui/react";
+import React from "react";
+import styled from "styled-components";
+import Navbar from "../Components/Navbar";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
-import { Search2Icon } from '@chakra-ui/icons'
-import   Slidercomp  from '../Components/Slider'
-import Multislider from '../Components/Multislider'
+import { Search2Icon } from "@chakra-ui/icons";
+import Slidercomp from "../Components/Slider";
+import Multislider from "../Components/Multislider";
 
 export default function Medicines() {
   return (
     <>
-    <Navbar/>
-    <DIV>
-    <div>
-   
-        <Box>
-            <Flex justifyContent={"space-between"} m={10} gap={"10px"} direction={["column", "column", "row", "row"]} >
-                <HStack width={"100%"} border={"1px solid gray"} p={1} pl={"2"} pr={"2"} borderRadius={"30px"} >
-                <Search2Icon/>   <Input m={[2,2]} variant={"unstyled"} type='search' placeholder="Search Madicine" /><Button p={3}  variant={"unstyled"}>Search</Button>
-                </HStack>
-                <HStack  width={"100%"}   justifyContent={"center"}>
-                    <Button width={"50%"} padding={4} gap={2} display={"flex"} bg={"#14BEF0"} borderRadius={0} color={"white"} variant={"unstyled"}><AiOutlineShoppingCart/>VIEW CART</Button>
-                </HStack>
+      <Navbar />
+      <DIV>
+        <div>
+          <Box>
+            <Flex
+              justifyContent={"space-between"}
+              m={10}
+              gap={"10px"}
+              direction={["column", "column", "row", "row"]}
+            >
+              <HStack
+                width={"100%"}
+                border={"1px solid gray"}
+                p={1}
+                pl={"2"}
+                pr={"2"}
+                borderRadius={"30px"}
+              >
+                <Search2Icon />{" "}
+                <Input
+                  m={[2, 2]}
+                  variant={"unstyled"}
+                  type="search"
+                  placeholder="Search Madicine"
+                />
+                <Button p={3} variant={"unstyled"}>
+                  Search
+                </Button>
+              </HStack>
+              <HStack width={"100%"} justifyContent={"center"}>
+                <Button
+                  width={"50%"}
+                  padding={4}
+                  gap={2}
+                  display={"flex"}
+                  bg={"#14BEF0"}
+                  borderRadius={0}
+                  color={"white"}
+                  variant={"unstyled"}
+                >
+                  <AiOutlineShoppingCart />
+                  VIEW CART
+                </Button>
+              </HStack>
             </Flex>
-            <Slidercomp/>
-        </Box>
-        <Heading textAlign={"left"} ml="5" as={"h2"}>Browse medicines & health products</Heading>
-            <Box boxSizing="border-box" m={[0,7,0,7]}>
-                <Multislider/>
-            </Box>
-    </div>
-
-    </DIV></>
-  )
+            <Slidercomp />
+          </Box>
+          <Heading textAlign={"left"} ml="5" as={"h2"}>
+            Browse medicines & health products
+          </Heading>
+          <Box boxSizing="border-box" m={[0, 7, 0, 7]}>
+            <Multislider />
+          </Box>
+         
+        </div>
+      </DIV>
+    </>
+  );
 }
-const DIV=styled.div`
-
-
-*{
-    font-family: 'Roboto', sans-serif;
-
-  
-}
-`
+const DIV = styled.div`
+  * {
+    font-family: "Roboto", sans-serif;
+  }
+`;
