44789d88563dadcd48212a14b8f3f7c305b971ed
prashant_rathour <prathour884@gmail.com>
2023-05-09 15:46:10 +0530

    ready for deploye

--- hospital-expertise/src/Pages/Admin.jsx
+++ hospital-expertise/src/Pages/Admin.jsx
@@ -1,151 +1,155 @@
-import { useState } from 'react';
+import { useState } from "react";
 import {
-	FormControl,
-	FormLabel,
-	FormErrorMessage,
-	Input,
-	Textarea,
-	Button,
-	VStack,
-	Image,
-	Box,
-	Heading,
-} from '@chakra-ui/react';
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+  Textarea,
+  Button,
+  VStack,
+  Image,
+  Box,Heading
+} from "@chakra-ui/react";
 
-import { useSelector, useDispatch, shallowEqual } from 'react-redux';
-import { postData } from '../Reducer/action';
-const initState = {
-	image: '',
-	professionalInput: '',
-	experienceInput: '',
-	locationInput: '',
-	feeInput: '',
-	name: '',
-};
+import {useSelector,useDispatch,shallowEqual} from 'react-redux'
+import { postData } from "../Reducer/action";
+const initState={
+    image: "",
+    professionalInput: "",
+    experienceInput: "",
+    locationInput: "",
+    feeInput: "",
+    name:""
+  }
 export function Admin() {
-	const [formData, setFormData] = useState(initState);
-	const [formError, setFormError] = useState('');
-	const { isLoading, isError } = useSelector((state) => {
-		return {
-			isLoading: state.loading,
-			isError: state.error,
-		};
-	}, shallowEqual);
-	const dispatch = useDispatch();
-	const handleInputChange = (e) => {
-		console.log(e.target.value);
-		const { name, value } = e.target;
-		setFormData({ ...formData, [name]: value });
-	};
+  const [formData, setFormData] = useState(initState);
+  const [formError, setFormError] = useState("");
+   const {isLoading,isError}=useSelector((state)=>{
+    return {
+        isLoading : state.loading,
+        isError:state.error
+    }
+   },shallowEqual);
+   const dispatch=useDispatch()
+  const handleInputChange = (e) => {
+    console.log(e.target.value)
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		console.log('hey sagar');
-		dispatch(postData(formData));
-		setFormData(initState);
-	};
 
-	return (
-		<Box
-			// bgGradient="linear(to-r, teal.500, blue.500)"
-			minH='50vh'
-			w={'50%'}
-			py={12}
-			m='auto'
-			px={{ base: 4, lg: 8 }}>
-			<form onSubmit={handleSubmit}>
-				<VStack
-					spacing={4}
-					align='stretch'
-					bg='white'
-					p={6}
-					rounded='md'
-					boxShadow='md'
-					//   bgGradient="linear(to-t, #cfd9df, #e2ebf0)"
-				>
-					<Heading size='xl' mb={4} textAlign='center' color='black'>
-						ADD DOCTOR
-					</Heading>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hey sagar");
+    dispatch(postData(formData))
+    setFormData(initState)
+  };
 
-					<FormControl isRequired>
-						<FormLabel>Name</FormLabel>
-						<Input
-							type='text'
-							name='name'
-							value={formData.name}
-							onChange={handleInputChange}
-						/>
-					</FormControl>
+  return (
+    <Box
+    // bgGradient="linear(to-r, teal.500, blue.500)"
+      minH="50vh"
+      w={"50%"}
+      py={12}
+      m="auto"
+      px={{ base: 4, lg: 8 }}
+    >
+      <form onSubmit={handleSubmit}>
+        <VStack
+          spacing={4}
+          align="stretch"
+          bg="white"
+          p={6}
+          rounded="md"
+          boxShadow="md"
+        //   bgGradient="linear(to-t, #cfd9df, #e2ebf0)"
+        >
 
-					<FormControl isRequired>
-						<FormLabel>Professional</FormLabel>
-						<Input
-							type='text'
-							name='professionalInput'
-							value={formData.professionalInput}
-							onChange={handleInputChange}
-						/>
-					</FormControl>
+<Heading size="xl" mb={4} textAlign="center" color="black">
+          ADD DOCTOR
+        </Heading>
 
-					<FormControl isRequired>
-						<FormLabel>Experience</FormLabel>
-						<Textarea
-							name='experienceInput'
-							value={formData.experienceInput}
-							onChange={handleInputChange}
-						/>
-					</FormControl>
+        <FormControl isRequired>
+            <FormLabel>Name</FormLabel>
+            <Input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
+          </FormControl>
 
-					<FormControl isRequired>
-						<FormLabel>Location </FormLabel>
-						<Input
-							type='text'
-							name='locationInput'
-							value={formData.locationInput}
-							onChange={handleInputChange}
-						/>
-					</FormControl>
 
-					<FormControl isRequired>
-						<FormLabel>Fee</FormLabel>
-						<Input
-							type='number'
-							name='feeInput'
-							value={formData.feeInput}
-							onChange={handleInputChange}
-						/>
-					</FormControl>
+          <FormControl isRequired>
+            <FormLabel>Professional</FormLabel>
+            <Input
+              type="text"
+              name="professionalInput"
+              value={formData.professionalInput}
+              onChange={handleInputChange}
+            />
+          </FormControl>
 
-					<FormControl>
-						<FormLabel>Image</FormLabel>
-						<Input
-							type='text'
-							onChange={handleInputChange}
-							name='image'
-							value={formData.image}
-						/>
-					</FormControl>
+          <FormControl isRequired>
+            <FormLabel>Experience</FormLabel>
+            <Textarea
+              name="experienceInput"
+              value={formData.experienceInput}
+              onChange={handleInputChange}
+            />
+          </FormControl>
 
-					{formError && (
-						<FormErrorMessage color='red.500'>{formError}</FormErrorMessage>
-					)}
+          <FormControl isRequired>
+            <FormLabel>Location </FormLabel>
+            <Input
+              type="text"
+              name="locationInput"
+              value={formData.locationInput}
+              onChange={handleInputChange}
+            />
+          </FormControl>
 
-					<Button
-						type='submit'
-						colorScheme='blue'
-						px={8}
-						py={4}
-						fontSize='md'
-						fontWeight='bold'
-						rounded='full'
-						bgGradient='linear(to-r, blue.400, teal.300)'
-						_hover={{
-							bgGradient: 'linear(to-l, blue.400, teal.300)',
-						}}>
-						Submit
-					</Button>
-				</VStack>
-			</form>
-		</Box>
-	);
+          <FormControl isRequired>
+            <FormLabel>Fee</FormLabel>
+            <Input
+              type="number"
+              name="feeInput"
+              value={formData.feeInput}
+              onChange={handleInputChange}
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel>Image</FormLabel>
+            <Input type="text" onChange={handleInputChange}
+            name="image"
+            value={formData.image}
+           
+            />
+    
+          </FormControl>
+
+          {formError && (
+            <FormErrorMessage color="red.500">{formError}</FormErrorMessage>
+          )}
+
+          <Button
+            type="submit"
+            colorScheme="blue"
+            px={8}
+            py={4}
+            fontSize="md"
+            fontWeight="bold"
+            rounded="full"
+            bgGradient="linear(to-r, blue.400, teal.300)"
+            _hover={{
+              bgGradient: "linear(to-l, blue.400, teal.300)",
+            }}
+          >
+            Submit
+          </Button>
+        </VStack>
+      </form>
+    </Box>
+  );
 }
--- hospital-expertise/src/index.js
+++ hospital-expertise/src/index.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { SocketProvider } from './Callingapp/Providers/Socket';
-import { PeerProvider } from './Callingapp/Providers/Peer';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 import { Auth0Provider } from '@auth0/auth0-react';
-// import { store } from "./store";
-// import {Provider} from "react-redux";
-import { store } from './Reducer/store';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { store } from "./store";
+import { Provider } from "react-redux";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<Provider store={store}>
-		<ChakraProvider>
-			<BrowserRouter>
-				<SocketProvider>
-					<PeerProvider>
-						{' '}
-						<Auth0Provider
+  <Provider store={store}>
+    <ChakraProvider>
+      <BrowserRouter>
+       
+        <Auth0Provider
 							domain='dev-36jbpvwtgmgzxos3.us.auth0.com'
 							clientId='KiEBoFefnKpKFluCSu5lyiUidPNIwuLo'
 							authorizationParams={{
@@ -28,11 +23,9 @@ root.render(
 							}}>
 							<App />
 						</Auth0Provider>
-					</PeerProvider>
-				</SocketProvider>
-			</BrowserRouter>
-		</ChakraProvider>
-	</Provider>
+      </BrowserRouter>
+    </ChakraProvider>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
