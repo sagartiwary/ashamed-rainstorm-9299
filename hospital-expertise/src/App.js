50e38c083f5efaa2ca7666674a00fa855bed4dfd
Sagar tiwary <113369535+sagartiwary@users.noreply.github.com>
2023-05-09 18:13:21 +0530

    Update App.js

--- hospital-expertise/src/App.js
+++ hospital-expertise/src/App.js
@@ -1,15 +1,12 @@
 import "./App.css";
 import Allroutes from "./Components/Allroutes";
 import { Footer } from "./Components/Footer";
-import Navbar from "./Components/Navbar";
-import { SearchDoctor } from "./Components/SearchDoctor";
-// import { Admin } from "./Pages/Admin";
-// import Finddoctor from './Pages/Finddoctor';
+
 function App() {
   return (
     <div className="App">
       <Allroutes />
-      <Footer/>
+      <Footer />
       {/* <Admin/> */}
     </div>
   );
